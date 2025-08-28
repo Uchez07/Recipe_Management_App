777862722f8831bb02f027b9fd3b7b5aa19e1b7e
Gacheru <johngacheru533@gmail.com>
2025-08-28 13:56:26 +0300

    imported the min and mav valuevalidatorfor prep time,cookingtime and rating

--- Recipe/Recipe_App/models.py
+++ Recipe/Recipe_App/models.py
@@ -1,15 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+from djando.core import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ('Dessert', 'Dessert'),
+        ('Main Course', 'Main Course'),
+        ('Appetizer', 'Appetizer'),
+        ('Beverage', 'Beverage'),
+        ('Snack', 'Snack'),
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
-    title = models.CharField(max_length=30, null=False)
+    title = models.CharField(max_length=30, null=False,blank=False)
     description = models.TextField(max_length=50, null=True)
-    ingredients = models.CharField(max_length=500)
-    date_created = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now_add=True)
+    instructions = models.TextField(null=False, blank=False)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, null=True, blank=True)
+    preparation_time = models.IntegerField(
+        validators=[MinValueValidator(1)],  # Must be at least 1 minute
+        help_text="Time in minutes",
+        null=True, blank=True
+    )
+    cooking_time = models.IntegerField(
+        validators=[MinValueValidator(1)],  # Must be at least 1 minute
+        help_text="Time in minutes",
+        null=True, blank=True
+    )
+    servings = models.IntegerField(
+        validators=[MinValueValidator(1)],  # At least 1 serving
+        null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
