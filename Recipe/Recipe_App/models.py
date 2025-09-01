866956907247245eb55ae72fe1447e1599e5a40d
Gacheru <johngacheru533@gmail.com>
2025-09-01 10:21:59 +0300

    Made database migrationsto create database tables

--- Recipe/Recipe_App/models.py
+++ Recipe/Recipe_App/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from djando.core import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
