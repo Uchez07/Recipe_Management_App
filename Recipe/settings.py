73c208e43426112c26b4447fb5e68db6367f6fef
Gacheru <johngacheru533@gmail.com>
2025-09-04 13:35:28 +0300

    made migrations

--- Recipe/settings.py
+++ Recipe/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest_framework',
     'Recipe_App',
 ]
 
